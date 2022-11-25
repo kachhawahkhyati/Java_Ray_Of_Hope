cf36792bf5a996db9d307c629e6ccb5b6e9fb927
kachhawahkhyati <kachhawah.k@northeastern.edu>
2022-11-25 12:37:24 -0500

    Introduced Aopter Package and created java class with variable declaration

---
+++ RayOfHope/src/Business/Adopter/Adopter.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Adopter;
+
+/**
+ *
+ * @author khyati
+ */
+public class Adopter {
+
+    private String name;
+    private String ssn;
+    private String gender;
+    private String emailId;
+    private int age;
+    private int userId;
+    private String username;
+    private boolean flag = false;
+    private float annualIncome;
+}
