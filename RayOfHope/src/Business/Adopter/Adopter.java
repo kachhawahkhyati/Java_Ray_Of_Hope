cb37868cd9ce22145ae892e8135d91920c462871
kachhawahkhyati <kachhawah.k@northeastern.edu>
2022-11-25 12:38:50 -0500

    Implemented getter() and setter() methods for Adopter

--- RayOfHope/src/Business/Adopter/Adopter.java
+++ RayOfHope/src/Business/Adopter/Adopter.java
@@ -6,7 +6,7 @@
 package Business.Adopter;
 
 /**
- *
+ * 
  * @author khyati
  */
 public class Adopter {
@@ -20,4 +20,82 @@ public class Adopter {
     private String username;
     private boolean flag = false;
     private float annualIncome;
+       
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public float getAnnualIncome() {
+        return annualIncome;
+    }
+
+    public void setAnnualIncome(float annualIncome) {
+        this.annualIncome = annualIncome;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(userId);
+    }
+
 }
