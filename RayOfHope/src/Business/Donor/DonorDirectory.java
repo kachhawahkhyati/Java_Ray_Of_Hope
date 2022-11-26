88437238e9a7e62d05a617fda35a225411ffc4a9
kachhawahkhyati <kachhawah.k@northeastern.edu>
2022-11-26 15:26:50 -0500

    Implemented Donor Directory java class

---
+++ RayOfHope/src/Business/Donor/DonorDirectory.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Donor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author khyati
+ */
+public class DonorDirectory {
+    
+    List<Donor> donorList;
+    
+    public DonorDirectory() {
+        donorList = new ArrayList<Donor>();
+    }
+    
+    private static DonorDirectory donorDirectory;
+
+    public List<Donor> getDonorList() {
+        return donorList;
+    }
+
+    public void setDonorList(List<Donor> donorList) {
+        this.donorList = donorList;
+    }
+
+    public static DonorDirectory getDonorDirectory() {
+        return donorDirectory;
+    }
+
+    public static void setDonorDirectory(DonorDirectory donorDirectory) {
+        DonorDirectory.donorDirectory = donorDirectory;
+    }
+    
+}
