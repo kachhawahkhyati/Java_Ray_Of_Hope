8dc01785aa9ae41b2f7e8c2d43cdc781c87f87f0
kachhawahkhyati <kachhawah.k@northeastern.edu>
2022-11-27 11:05:02 -0500

    Created DB$ Util file

---
+++ RayOfHope/src/Business/DB4OUtil/DB4OUtil.java
@@ -0,0 +1,28 @@
+package Business.DB4OUtil;
+
+import Business.ConfigureASystem;
+import Business.EcoSystem;
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+import com.db4o.config.EmbeddedConfiguration;
+import com.db4o.ta.TransparentPersistenceSupport;
+import java.nio.file.Paths;
+
+/**
+ *
+ *
+ * @author khyati
+ */
+public class DB4OUtil {
+
+    private static final String FILENAME = Paths.get("Databank.db4o").toAbsolutePath().toString();// path to the data store
+    private static DB4OUtil dB4OUtil;
+    
+    public synchronized static DB4OUtil getInstance(){
+        if (dB4OUtil == null){
+            dB4OUtil = new DB4OUtil();
+        }
+        return dB4OUtil;
+    }
+}
