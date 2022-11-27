dbba0ed09f39c720415f7c0aaf156fe2f2a7a0bf
kachhawahkhyati <kachhawah.k@northeastern.edu>
2022-11-27 10:32:22 -0500

    Creation of Person java class

---
+++ RayOfHope/src/Business/Person/Person.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Person;
+
+import java.util.Date;
+
+/**
+ *
+ * @author khyati
+ */
+public class Person {
+
+    int personId;
+    String name;
+    int personAge;
+    String status;
+    String imageDetails;
+    String identificationMark;
+    String gender;
+    private static int count = 1;
+    Date registrationDate;
+    String medicalStatus;
+    private double BP;
+    private double pulseRate;
+    private double respirationRate;
+    private double bodytemp;
+    private boolean financialHelp;
+    private String amt;
+    private boolean medicalHelp;
+    private boolean isSpecialPerson;
+
+  
+}
