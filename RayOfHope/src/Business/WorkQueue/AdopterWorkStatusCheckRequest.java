505d8c6a31da28dfb0c557f2fc81c0808bfc563f
kachhawahkhyati <kachhawah.k@northeastern.edu>
2022-11-28 14:37:15 -0500

    Generated getter and setter methods for AdopterWorkSTatusCheckRequest.java

--- RayOfHope/src/Business/WorkQueue/AdopterWorkStatusCheckRequest.java
+++ RayOfHope/src/Business/WorkQueue/AdopterWorkStatusCheckRequest.java
@@ -14,4 +14,27 @@ public class AdopterWorkStatusCheckRequest extends WorkRequest {
      private String financeStatus;
      private String personCareStatus;
 
-   }
+    public String getBgcStatus() {
+        return bgcStatus;
+    }
+    public String getFinanceStatus() {
+        return financeStatus;
+    }
+
+    public void setFinanceStatus(String financeStatus) {
+        this.financeStatus = financeStatus;
+    }
+
+    public void setBgcStatus(String bgcStatus) {
+        this.bgcStatus = bgcStatus;
+    }
+    public String getPersonCareStatus() {
+        return personCareStatus;
+    }
+
+    public void setPersonCareStatus(String personCareStatus) {
+        this.personCareStatus = personCareStatus;
+
+    
+    
+}}
