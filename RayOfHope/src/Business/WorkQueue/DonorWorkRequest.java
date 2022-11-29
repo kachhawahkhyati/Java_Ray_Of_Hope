1b36ae2bc4137eba7c5ba1ab8f4000698e57e349
kachhawahkhyati <kachhawah.k@northeastern.edu>
2022-11-29 18:54:57 -0500

    Implemented Donor Work Request file

---
+++ RayOfHope/src/Business/WorkQueue/DonorWorkRequest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.WorkQueue;
+
+/**
+ *
+ * @author khyati
+ */
+public class DonorWorkRequest extends WorkRequest {
+    
+     private String userName;
+    private String amount;
+    private String emailId;
+    private String Remarks;
+   
+
+    
+}
